fd38657aadbbb143bf00defd1c8691d5ad50bf22
aryan-ydv <aryanyduvanshi03@gmail.com>
2021-05-25 11:01:58 +0530

    new

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const express =require("express");
+const express = require("express");
 require("./db/config");
-const cors=require("cors");
-const port =process.env.PORT || 3000;
+const cors = require("cors");
+const port = process.env.PORT || 3000;
 let AddSalon = require("./models/addSalon");
-const app=express();
+const app = express();
 require("dotenv").config();
 
-
-app.use(express.json());
-app.use(cors());
-
 //to see all our data route
-app.get("/", (req, res) => {
-    AddSalon.find()
+app.get("/get", (req, res) => {
+  AddSalon.find()
     .then((exercise) => {
       res.json(exercise);
     })
@@ -23,77 +19,76 @@ app.get("/", (req, res) => {
 
 //for creating new addsalon data route
 app.post("/addsalon", (req, res) => {
-        const   timings =req.body.timings;
-        const  std =req.body.std;
-        const state =req.body.state;
-        const skinCareGentsServices =req.body.skinCareGentsServices;
-        const salonName= req.body.salonName;
-        const salonEmail=req.body.salonEmail;
-        const  pinCode=req.body.pinCode;
-        const phoneNumber=req.body.phoneNumber;
-        const payment=req.body.payment;
-        const owner=req.body.owner;
-        const openStatus=req.body.openStatus;
-        const  nailCareLadiesServices =req.body.nailCareLadiesServices;
-        const  makeUpLadiesServices=req.body.makeUpLadiesServices;
-        const locationCity=req.body.locationCity;
-        const handAndFeetLadiesServices=req.body.handAndFeetLadiesServices;
-        const  hairTreatmentsGentsServices=req.body.hairTreatmentsGentsServices;
-        const  hairTreatmentLadiesServices=req.body.hairTreatmentLadiesServices;
-        const hairTextureLadiesServices=req.body.hairTextureLadiesServices;
-        const  hairTextureGentsServices = req.body.hairTextureGentsServices;
-        const  hairStylingLadiesServices=req.body.hairStylingLadiesServices;
-        const  hairCutAndFinishGentsServices=req.body.hairCutAndFinishGentsServices;
-        const hairColourGentsServices=req.body.hairColourGentsServices;
-        const  facilities=req.body.facilities
-        const  facialsAndRitualsLadiesServices=req.body.facialsAndRitualsLadiesServices;
-        const  beardGroomingGentsServices=req.body.beardGroomingGentsServices;
-        const address=req.body.address;
-        const salonWebsite=req.body.salonWebsite;
-        const  location=req.body.location;
+  const timings = req.body.timings;
+  const std = req.body.std;
+  const state = req.body.state;
+  const skinCareGentsServices = req.body.skinCareGentsServices;
+  const salonName = req.body.salonName;
+  const salonEmail = req.body.salonEmail;
+  const pinCode = req.body.pinCode;
+  const phoneNumber = req.body.phoneNumber;
+  const payment = req.body.payment;
+  const owner = req.body.owner;
+  const openStatus = req.body.openStatus;
+  const nailCareLadiesServices = req.body.nailCareLadiesServices;
+  const makeUpLadiesServices = req.body.makeUpLadiesServices;
+  const City = req.body.City;
+  const handAndFeetLadiesServices = req.body.handAndFeetLadiesServices;
+  const hairTreatmentsGentsServices = req.body.hairTreatmentsGentsServices;
+  const hairTreatmentLadiesServices = req.body.hairTreatmentLadiesServices;
+  const hairTextureLadiesServices = req.body.hairTextureLadiesServices;
+  const hairTextureGentsServices = req.body.hairTextureGentsServices;
+  const hairStylingLadiesServices = req.body.hairStylingLadiesServices;
+  const hairCutAndFinishGentsServices = req.body.hairCutAndFinishGentsServices;
+  const hairColourGentsServices = req.body.hairColourGentsServices;
+  const facilities = req.body.facilities;
+  const facialsAndRitualsLadiesServices =
+    req.body.facialsAndRitualsLadiesServices;
+  const beardGroomingGentsServices = req.body.beardGroomingGentsServices;
+  const address = req.body.address;
+  const salonWebsite = req.body.salonWebsite;
+  const location = req.body.location;
 
-      const addsalon= new AddSalon({
-        timings,
-        std,
-        state,
-        skinCareGentsServices,
-        salonName,
-        salonEmail,
-        pinCode,
-        phoneNumber,
-        payment,
-        owner,
-        openStatus,
-        nailCareLadiesServices,
-        makeUpLadiesServices,
-        locationCity,
-        handAndFeetLadiesServices,
-        hairTreatmentsGentsServices,
-        hairTreatmentLadiesServices,
-        hairTextureLadiesServices,
-        hairTextureGentsServices,
-        hairStylingLadiesServices,
-        hairCutAndFinishGentsServices,
-        hairColourGentsServices,
-        facilities,
-        facialsAndRitualsLadiesServices,
-        beardGroomingGentsServices,
-        address,
-        salonWebsite,
-        location,    //28
-      });
-      addsalon.save()
-      .then(()=>{
-          res.json("salon data is added!")
-      })
-      .catch((err)=>{
-          res.json(err);
-      })
-
+  const addsalon = new AddSalon({
+    timings,
+    std,
+    state,
+    skinCareGentsServices,
+    salonName,
+    salonEmail,
+    pinCode,
+    phoneNumber,
+    payment,
+    owner,
+    openStatus,
+    nailCareLadiesServices,
+    makeUpLadiesServices,
+    City,
+    handAndFeetLadiesServices,
+    hairTreatmentsGentsServices,
+    hairTreatmentLadiesServices,
+    hairTextureLadiesServices,
+    hairTextureGentsServices,
+    hairStylingLadiesServices,
+    hairCutAndFinishGentsServices,
+    hairColourGentsServices,
+    facilities,
+    facialsAndRitualsLadiesServices,
+    beardGroomingGentsServices,
+    address,
+    salonWebsite,
+    location,
+  });
+  addsalon
+    .save()
+    .then(() => {
+      res.json({ message: "salon data is added!" });
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
-app.listen(port,()=>{
-    console.log(`app is running on ${port}`);
-})
-
-
+app.listen(port, () => {
+  console.log(`app is running on ${port}`);
+});
--- models/addSalon.js
+++ models/addSalon.js
@@ -15,7 +15,7 @@ const addSalonSchema=new Schema({
     openStatus: {type: String},
     nailCareLadiesServices: {type: String},
     makeUpLadiesServices: {type: String},
-    locationCity: {type: String},
+    City: {type: String},
     handAndFeetLadiesServices: {type: String},
     hairTreatmentsGentsServices: {type: String},
     hairTreatmentLadiesServices: {type: String},
@@ -104,19 +104,4 @@ const addSalonSchema=new Schema({
 
 
 
- //const {auth, requiresAuth} =require("express-openid-connect");
 
-// app.use(
-//     auth({
-//         authRequired: false,
-//         auth0Logout: true,
-//       issuerBaseURL: process.env.ISSUER_BASE_URL,
-//       baseURL: process.env.BASE_URL,
-//       clientID: process.env.CLIENT_ID,
-//       secret: process.env.SECRET,
-//     })
-//   );
-
-//   app.get("/",(req,res)=>{
-//       res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-//   })
